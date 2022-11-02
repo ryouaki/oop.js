6ba8bfb67d4b6274f9c53d001c9de7f33184701a
LiangYaHui <46517115@qq.com>
2022-11-02 16:21:30 +0800

    玩

---
+++ src/App.js
@@ -0,0 +1,7 @@
+import {Application} from './../lib/index';
+
+export default class extends Application {
+  constructor(node) {
+    super(node);
+  }
+}
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import App from './App.js';
+
+const app = new App({
+  root: document.getElementById('root')
+})
+
+app.doRender();
